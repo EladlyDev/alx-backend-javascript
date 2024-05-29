1b51f7e816d76dc7e79d5336d5a5a5ce298b3df2
Mohamed Eladly <msrtv365@yahoo.com>
2024-05-29 12:21:52 +0300

    Done with the advanced task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let out = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    out = { photo, user };
+  } catch (err) {
+    out = { photo: null, user: null };
+  }
+
+  return out;
+}
