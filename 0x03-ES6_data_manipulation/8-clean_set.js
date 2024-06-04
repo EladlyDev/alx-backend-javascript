7f689c8c9c4f750b71f571dafa489fffe4957b6c
Mohamed Eladly <msrtv365@yahoo.com>
2024-06-04 12:50:40 +0300

    Done with task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) { return ''; }
+  return Array.from(set)
+    .filter((e) => typeof e === 'string' && e.startsWith(startString))
+    .map((e) => e.slice(startString.length))
+    .join('-');
+}
---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,8 @@
+export default function groceriesList() {
+    const list = `Apples, 10
+    Tomatoes, 10
+    Pasta, 1
+    Rice, 1
+    Banana, 5`.split(', ');
+    console.log(list);
+}
