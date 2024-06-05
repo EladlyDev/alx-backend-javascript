ce2d25ebd6226f311b3dc641bcf80519be3eb5de
Mohamed Eladly <msrtv365@yahoo.com>
2024-06-05 15:28:25 +0300

    Done with task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,29 @@ interface printTeacherFunction {
 }
 
 const printTeacher : printTeacherFunction = (firstName, lastName) => `${firstName[0]}. ${lastName}`;
+
+interface studentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+interface studentClassConstructurInterface {
+    new (firstName: string, lastName: string): studentClassInterface;
+}
+
+class StudentClass implements studentClassInterface {
+    private _firstName!: string;
+    private _lastName!: string;
+
+    constructur(firstName: string, lastName: string) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+    
+    displayName() {
+        return this._firstName;
+    }
+}
